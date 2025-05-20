524c08ded831504f8e31dc10d980842d42d217e0
Govind9066 <gadekargovind575@gmail.com>
2025-05-20 20:47:03 +0530

    create a variable add use it purely in the react

--- Basic_React_app/src/UsingOfCurlyBreses.jsx
+++ Basic_React_app/src/UsingOfCurlyBreses.jsx
@@ -1,7 +1,11 @@
 function Mul(){
+    let name="Govind";
     return(
+        <>
         <h2>2 * 2={2*2}</h2>
-    )
+        <p>Hi, {name.toUpperCase()} welcome to the react</p>
+        </>
+    );
 }
 
 export default Mul;
