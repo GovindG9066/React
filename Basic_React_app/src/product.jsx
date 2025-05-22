cad9edde889de31148958fa93f595ba3e89b1756
Govind9066 <gadekargovind575@gmail.com>
2025-05-22 18:33:41 +0530

    use the style in container

--- Basic_React_app/src/product.jsx
+++ Basic_React_app/src/product.jsx
@@ -1,9 +1,11 @@
+import ProductTab from "./ProductTab";
+
 function Product(){
     return(
-        <>
+        <div className="product">
         <h3>Product Title</h3>
         <h4>Product Desc</h4>
-        </>
+        </div>
     );
 }
 
