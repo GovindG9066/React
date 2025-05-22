820c7ee2356111be152d90165cc250cdebab3006
Govind9066 <gadekargovind575@gmail.com>
2025-05-22 19:56:52 +0530

    complet the arrays and objects

--- Basic_React_app/src/App.jsx
+++ Basic_React_app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Item title="ABC" price={20000}/>
+      <Item title="ABC" price={20000} />
       <Item title="SONA" price={10000}/>
       <Item title="KHU" price={3000}/>
       <Item title="XYZ" price={100}/>
--- Basic_React_app/src/item.jsx
+++ Basic_React_app/src/item.jsx
@@ -1,10 +1,12 @@
 import "./item.css";
 function Item({title,price}) {
+    let options=["fast","slow","Durable"];
     return (
         <div className="item">
 
             <h2>{title}</h2>
             <h3>{price}</h3>
+            <p>features={options}</p>
 
         </div>
     );
