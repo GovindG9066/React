03a68a376a3a3b7402d473c998a8ab8cb6bc53bb
Govind9066 <gadekargovind575@gmail.com>
2025-06-02 22:43:12 +0530

    try something new in react use the props

--- Basic_React_app/src/item.jsx
+++ Basic_React_app/src/item.jsx
@@ -1,12 +1,13 @@
 import "./item.css";
-function Item({title,price}) {
-    let options=["fast","slow","Durable"];
+function Item({title,price,feature}) {
+    // let options=["fast","slow","Durable"];
+    let list=feature.map=((feature)=><li>{feature}</li>)
     return (
         <div className="item">
 
             <h2>{title}</h2>
             <h3>{price}</h3>
-            <p>features={options}</p>
+            <p>features={list}</p>
 
         </div>
     );
