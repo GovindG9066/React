9585f52cdcd1083575a115907bd232f0ff6a57bd
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 17:49:36 +0530

    use the dynamic component styling and use the condition bg color

--- Basic_React_app/src/newProduct.jsx
+++ Basic_React_app/src/newProduct.jsx
@@ -3,16 +3,18 @@ import "./newProduct.css"
 function NewProduct({name,image,desc,amount}) {
     let options=["fresh, ","durable, ","testy"];
     const list=options.map((options)=><li>{options}</li>)
+    const isDiscount=amount>=100;
+    let styles={backgroundColor:isDiscount ? "green":""};
     
     return(
-       <div className="container">
+       <div className="container" style={styles}>
             <h1 className="ProductName">{name}</h1>
             <img src={image} className="ProductImg" alt="" />
             <p className="ProductDesc">{desc}</p>
             <p className="list">options-{list}</p>
             <button className="btn amountbtn">price- ${amount}</button>
             <button className="btn addtocartbtn">Add to Cart</button>
-            {amount >= 100 ? <p className="discount">Discount of 5%</p> :null }
+            {isDiscount ? <p className="discount">Discount of 5%</p> :null }
        </div>
     );
 }
