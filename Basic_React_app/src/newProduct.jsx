71c0c0482c550e1808bee417afdf522dd9096b65
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 16:14:33 +0530

    revise the array in the props and add the array and object in it

--- Basic_React_app/src/newProduct.jsx
+++ Basic_React_app/src/newProduct.jsx
@@ -1,11 +1,13 @@
 import "./newProduct.css"
 
 function NewProduct({name,image,desc,amount}) {
+    let options=["fresh, ","durable, ","testy"];
     return(
        <div className="container">
             <h1 className="ProductName">{name}</h1>
             <img src={image} className="ProductImg" alt="" />
             <p className="ProductDesc">{desc}</p>
+            <p>options-{options}</p>
             <button className="btn amountbtn">price-{amount}</button>
             <button className="btn addtocartbtn">Add to Cart</button>
        </div>
