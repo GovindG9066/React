19d9e4b8a635d6702f0445a20b69c5cefc40621e
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 20:56:12 +0530

    non click events onMouseOver

--- Basic_React_app/src/ClickEvents.jsx
+++ Basic_React_app/src/ClickEvents.jsx
@@ -2,11 +2,16 @@ function ClickBtn() {
     console.log("Hello world");
     
 }
+function nowHover(params) {
+    console.log("I am not Click");
+    
+}
 
 export default function ClickEvents() {
     return(
         <div>
             <button onClick={ClickBtn}>Click me</button>
+            <p onMouseOver={nowHover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt eaque error odio quia unde deleniti culpa facere ut quasi sapiente exercitationem ad, nobis ipsum perspiciatis architecto veritatis autem ea! Cum?</p>
         </div>
     );
 }
