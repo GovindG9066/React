1524a2c2c9652972f607b59a83134fc100a525be
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 22:38:52 +0530

    counter ko modify kiya

--- Basic_React_app/src/counter.jsx
+++ Basic_React_app/src/counter.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 
 export default function Counter() {
-let [count,setCount]=useState(0);
+    const [count, setCount] = useState(0);
 
-function IncCount() {
-    setCount(count+1)
+    function IncCounter() {
+        setCount(count + 1);
+    }
 
-};
-return(
-    <div>
-        <p>Count-{count}</p>
-        <button onClick={IncCount}> Increase Counter </button>
-    </div>
-);
-
-
-};
+    return (
+        <div>
+            <p>count= {count}</p>
+            <button onClick={IncCounter}>Click me to increase count</button>
+        </div>
+    );
+}
