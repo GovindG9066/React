0261ff9de77f76d83499de433d01962aa75ae8aa
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 21:02:02 +0530

    double click tested

--- Basic_React_app/src/ClickEvents.jsx
+++ Basic_React_app/src/ClickEvents.jsx
@@ -2,8 +2,12 @@ function ClickBtn() {
     console.log("Hello world");
     
 }
-function nowHover(params) {
+function nowHover() {
     console.log("I am not Click");
+    
+}
+function DoubleClickme() {
+    console.log("I was click twice");
     
 }
 
@@ -12,6 +16,7 @@ export default function ClickEvents() {
         <div>
             <button onClick={ClickBtn}>Click me</button>
             <p onMouseOver={nowHover}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt eaque error odio quia unde deleniti culpa facere ut quasi sapiente exercitationem ad, nobis ipsum perspiciatis architecto veritatis autem ea! Cum?</p>
+            <button onDoubleClick={DoubleClickme}>Click me Twice</button>
         </div>
     );
 }
