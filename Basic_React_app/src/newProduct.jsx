18cbaf4a8f1df3c090ab0533b10ab5dfca2085f7
Govind9066 <gadekargovind575@gmail.com>
2025-06-08 16:33:59 +0530

    map method ko use  kiya direct <li> ke aandar insert krr ke dekh liya working samaj liya

--- Basic_React_app/src/newProduct.jsx
+++ Basic_React_app/src/newProduct.jsx
@@ -2,12 +2,13 @@ import "./newProduct.css"
 
 function NewProduct({name,image,desc,amount}) {
     let options=["fresh, ","durable, ","testy"];
+    const list=options.map((options)=><li>{options}</li>)
     return(
        <div className="container">
             <h1 className="ProductName">{name}</h1>
             <img src={image} className="ProductImg" alt="" />
             <p className="ProductDesc">{desc}</p>
-            <p>options-{options}</p>
+            <p className="list">options-{list}</p>
             <button className="btn amountbtn">price-{amount}</button>
             <button className="btn addtocartbtn">Add to Cart</button>
        </div>
