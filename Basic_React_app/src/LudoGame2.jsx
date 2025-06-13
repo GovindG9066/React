4e0231ecd2651dc158d079cec3215edac6092289
Govind9066 <gadekargovind575@gmail.com>
2025-06-13 22:17:57 +0530

    trying to add new LudoGame

---
+++ Basic_React_app/src/LudoGame2.jsx
@@ -0,0 +1,17 @@
+
+
+export default function LudoGame2(){
+    return(
+        <div>
+            
+            <button>+1</button>
+            <p>Green Move</p>
+            <button>+1</button>
+            <p>red Move</p>
+            <button>+1</button>
+            <p>blue Move</p>
+            <button>+1</button>
+            <p>yellow Move</p>
+        </div>
+    );
+}
